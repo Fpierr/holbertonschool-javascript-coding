9027276f06a5ee73559e22a409bafca3b5f7b980
Fpierr <fanuelp9@gmail.com>
2024-05-13 21:16:12 +0200

    read a file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const filePath = process.argv[2];
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) { console.log(err); } else { consome.log(data); }
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) { console.log(err); } else { console.log(data); }
 });
