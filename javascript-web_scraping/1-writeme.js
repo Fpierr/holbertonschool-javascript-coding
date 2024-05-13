15ef7b3cc4c861b0fefa60c110fd7f1f9010dfbf
Fpierr <fanuelp9@gmail.com>
2024-05-13 21:43:38 +0200

    write in a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,5 +4,5 @@ const filePath = process.argv[2];
 const content = process.argv[3];
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
-    console.log(err);
+    console.log(err); }
 });
